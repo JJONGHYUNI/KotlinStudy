6e8178735409b2687b80556c639bd1af714d89d0
JJONGHYUNI <whdgusdl8792@gmail.com>
2022-08-10 22:18:01 +0900

    codelab 3-3

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -75,11 +76,11 @@ class GameFragment : Fragment() {
                         binding.invalidateAll()
                     } else {
                         view.findNavController()
-                            .navigate(R.id.action_gameFragment_to_gameWonFragment2)
+                            .navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment2(numQuestions,questionIndex))
                     }
                 } else {
                     view.findNavController().
-                            navigate(R.id.action_gameFragment_to_gameOverFragment)
+                            navigate(GameFragmentDirections.actionGameFragmentToGameOverFragment())
                 }
             }
         }
