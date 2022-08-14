bf6b121cb6c60ca1a50bb447057592c9381e7f21
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2022-08-14 23:03:03 +0900

    live data 사용

--- GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,18 +1,20 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
     // The current word
-    var word = ""
+    val word = MutableLiveData<String>()
+    val score = MutableLiveData<Int>()
 
-    // The current score
-    var score = 0
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
     init{
+        word.value = ""
+        score.value=0
         resetList()
         nextWord()
         Log.i("GameViewModel","GameViewModel created!")
@@ -44,19 +46,19 @@ class GameViewModel: ViewModel() {
         wordList.shuffle()
     }
     fun onSkip() {
-        score--
+        score.value=(score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+        score.value=(score.value)?.plus(1)
         nextWord()
     }
 
     private fun nextWord() {
         if (!wordList.isEmpty()) {
             //Select and remove a word from the list
-            word = wordList.removeAt(0)
+            word.value = wordList.removeAt(0)
         }
     }
     override fun onCleared() {
